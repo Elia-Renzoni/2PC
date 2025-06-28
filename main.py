cb22fe5857ce22bbd50564df169e635616a19621
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-06-28 11:34:55 +0200

    [ADD]: basic endpoints

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("")
+def execute_client_tx():
+    pass
+
+@app.post("")
+def execute_leader_election():
+    pass
+
+@app.post("")
+def execute_join_cluster(): 
+    pass
