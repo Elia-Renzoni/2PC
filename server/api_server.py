3bd40232013ffb17e5674bd99e9749abcb5bf098
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-07-06 15:36:10 +0200

    [ADD]: mod

--- server/api_server.py
+++ server/api_server.py
@@ -1,4 +1,8 @@
+from cluster.cluster_conf_manager import ConfManager
+
 
 class Server:
-    def __init__(self):
-        pass
+    def __init__(self, address: str, port: int, ):
+        self.addr = address
+        self.port = port
+
