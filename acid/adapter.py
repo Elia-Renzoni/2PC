e899c174d144cc34dfa3f057fc7a5080470a8f1a
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-07-12 16:43:28 +0200

    [ADD]: local transaction executor

--- acid/adapter.py
+++ acid/adapter.py
@@ -2,4 +2,21 @@ import sqlite3
 
 class DataAdapter:
     def __init__(self):
+        self.conn = sqlite3.connect("file.db")
+        self.cursor = self.conn.cursor()
+
+    def execute_local_tx(self, transaction: list[str]):
+        try:
+            for tx in transaction:
+                self.cursor.execute(tx)
+        except Exception as e:
+            self.conn.rollback()
+            # TODO -> contact the leader
+        finally:
+            # TODO -> contact the leader in case of commit
+            pass
+        
+
+
+    def call_the_leader(self):
         pass
