422141016ed9bd86eeba247402a217eec206e681
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-07-12 17:15:17 +0200

    [UPDATE]: distributed committer

--- acid/adapter.py
+++ acid/adapter.py
@@ -1,17 +1,24 @@
 import sqlite3
+from acid.two_phase_commit import DistributedCommit
 
 class DataAdapter:
-    def __init__(self):
+    def __init__(self, committer: DistributedCommit):
         self.conn = sqlite3.connect("file.db")
         self.cursor = self.conn.cursor()
+        self.leader_forwarder = committer
+        self.result = False
 
     def execute_local_tx(self, transaction: list[str]):
         try:
             for tx in transaction:
                 self.cursor.execute(tx)
         except Exception as e:
-            self.conn.rollback()
-            # TODO -> contact the leader
+            self.result = self.leader_forwarder.forward_result_to_leader("rollback")
+            if self.result is False:
+                self.conn.rollback()
         finally:
-            # TODO -> contact the leader in case of commit
-            pass
+            self.result = self.leader_forwarder.forward_result_to_leader("commit")
+            if self.result is True:
+                self.conn.commit()
+            else:
+                self.conn.rollback()
--- acid/two_phase_commit.py
+++ acid/two_phase_commit.py
@@ -1,10 +1,38 @@
 import sqlite3
 import requests
+from cluster.cluster_leader_election import LeaderElection
+
+HTTP_PROTO = "http"
+HTTP_TARGET_ENDPOINT = "/results"
+LEADER_COMMIT = "commit"
+LEADER_ROLLBACK = "rollback"
 
 class DistributedCommit:
-    def __init__(self):
-        pass
+    def __init__(self, leader_manager: LeaderElection):
+        self.url = None
+        self.data = None
+        self.leader_decision = None
+        self.response = None
+        self.leader_m = leader_manager
+        self.leader_addr = None
 
-    def forward_result_to_leader(self):
-        pass
+    def forward_result_to_leader(self, execution_result: str) -> bool:
+        # TODO -> get leader address
+        addr, port, _ = self.leader_m.get_monarchical_leader()
+        self.leader_addr = "{}:{}".format(addr, port)
+        self.make_forward_request(self.leader_addr, execution_result)
+
+        self.response = requests.post(self.url, data=self.data)
+        self.leader_decision = self.response.json()
+        if self.leader_decision is LEADER_COMMIT:
+            return True
+        return False
+
+    def make_forward_request(self, leader_addr: str, local_execution_result: str):
+        self.url = "{}://{}{}".format(HTTP_PROTO,
+                                      leader_addr,
+                                      HTTP_TARGET_ENDPOINT)
+        self.data = {
+            "tx_result": local_execution_result
+        }  
 
